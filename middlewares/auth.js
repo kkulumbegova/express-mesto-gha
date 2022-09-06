a56f16f80d74ac6c613826b535e3b430b5e8813f
Kate Kulumbegova <ekulumbegova@yandex.ru>
2022-09-06 22:29:07 +1000

    Проблема с ключами решена

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, 'secret-key');
+    payload = jwt.verify(token, 'dev-secret');
   } catch (err) {
     return next(new UnautorizedError('Необходима авторизация'));
   }
