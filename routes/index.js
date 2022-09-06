1e8efb1bda142b51847194f1c7cc0a600abd01fc
Kate Kulumbegova <ekulumbegova@yandex.ru>
2022-09-06 23:21:54 +1000

    Обработка ошибок

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,12 @@ const cardsRouter = require('./cards');
 const { signupValidation, signinValidation } = require('../middlewares/validation');
 const NotFound = require('../errors/not-found-err');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post('/signup', signupValidation, createUser);
 router.post('/signin', signinValidation, login);
 
